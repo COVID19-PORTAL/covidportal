6085a8b17dc872e3928d117f0b5d2cd666205d5f
gondhes <fajarwirazdi@gmail.com>
2021-03-04 16:20:00 +0700

    Inisiate Project

---
+++ server/controllers/index.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const {User} = require('../models')
+const {comparePassword} = require('../helpers/passwordHelper')
+
+class Controller {
+
+    static register(req, res) {
+       
+    }
+
+    static login(req, res) {
+        
+    }
+
+    static findAll(req, res) {
+        
+    }
+
+}
+
+module.exports = Controller
